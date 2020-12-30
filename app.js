b93430141e1f4872a08df238cc2ef303aabaffb0
TheMrPlus <mr.plus.co@gmail.com>
2020-12-30 01:06:22 +0000

    new req status

--- app.js
+++ app.js
@@ -7,3 +7,9 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+app.get('/master', (req,res) -> {
+	res.sent('X-full-stack', '4life');
+	res.status(418);
+	res.send('Santiago Sarabia Young God');
+});
